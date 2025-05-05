ecb23a9e89572f9942c8e94fcd37465246d7f871
Saacman <saacman27@gmail.com>
2025-05-05 12:46:26 +0200

    Add solution for <<Always Blocks Clocked>>

---
+++ verilog_language/procedures/29_always_blocks_clocked.v
@@ -0,0 +1,19 @@
+// synthesis verilog_input_version verilog_2001
+module top_module(
+    input clk,
+    input a,
+    input b,
+    output wire out_assign,
+    output reg out_always_comb,
+    output reg out_always_ff   );
+    // assign statement
+    assign out_assign = a ^ b;
+    // combinational always block
+    always @(*) begin
+        out_always_comb = a ^ b;
+    end
+    // clocked always block
+    always @(posedge clk) begin
+        out_always_ff <= a ^ b;
+    end
+endmodule
