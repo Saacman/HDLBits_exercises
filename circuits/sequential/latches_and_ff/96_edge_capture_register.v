9505fecbf22add90a97ace2e5dac6bc83fe507d7
Saacman <saacman27@gmail.com>
2025-07-05 19:05:29 +0200

    Add solution for <<Edge Capture Register>>

---
+++ circuits/sequential/latches_and_ff/96_edge_capture_register.v
@@ -0,0 +1,18 @@
+module top_module (
+    input clk,
+    input reset,
+    input [31:0] in,
+    output [31:0] out
+);
+    reg [31:0] d;
+    
+    always @(posedge clk) begin
+        if(reset)
+            out <= 32'b0;
+        else begin
+            out <= out | (~in & d);
+            
+        end
+        d <= in;
+    end
+endmodule
