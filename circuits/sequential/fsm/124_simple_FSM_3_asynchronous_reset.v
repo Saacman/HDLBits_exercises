429faf7dab911588a3bcd4687eb5a53b34df00e4
Saacman <saacman27@gmail.com>
2025-07-22 13:57:34 +0200

    Add solution for <<Simple Fsm 3 Asynchronous Reset>>

---
+++ circuits/sequential/fsm/124_simple_FSM_3_asynchronous_reset.v
@@ -0,0 +1,27 @@
+module top_module(
+    input clk,
+    input in,
+    input areset,
+    output out);
+
+    parameter A=2'b00, B=2'b01, C=2'b10, D=2'b11;
+    reg [1:0] state, next_state;
+    // State transition logic: next_state = f(state, in)
+    always @(*) begin
+        case (state)
+            A: next_state = in? B: A;
+            B: next_state = in? B: C;
+            C: next_state = in? D: A;
+            D: next_state = in? B: C;
+        endcase
+    end
+    
+    always @(posedge clk, posedge areset) begin
+        if (areset)
+            state <= A;
+        else
+            state <= next_state;
+    end
+    // Output logic:  out = f(state) for a Moore state machine
+    assign out = (state == D);
+endmodule
